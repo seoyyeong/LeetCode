ee874b068e3fb513aa9198a11603662bcf86fee2
seoyyeong <118430395+seoyyeong@users.noreply.github.com>
2023-10-19 17:03:23 +0900

    Time: 9 ms (25.65%), Space: 8.1 MB (81.59%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        string ans;
+        string temp;
+        
+        stack<string> st; 
+        int size = path.size();
+        int idx = 0;
+        int cnt = 0;
+        
+        for(int i=0;i<size;i++)
+        {
+            if(path[i]=='/')
+            {
+                st.push(path.substr(idx, i - idx));
+                idx = i;
+            }
+        }
+        
+        if(idx!=size-1)
+        {
+            st.push(path.substr(idx,size-idx));
+        }
+        
+        while(st.size()!=0)
+        {  
+            if(st.top()=="/")
+            {
+                st.pop();
+            }
+            else if(st.top()=="/.")
+            {
+                st.pop();
+            }
+            else if(st.top()=="/..")
+            {
+                cnt++;
+                st.pop();  
+            }
+            else
+            {
+                if(cnt==0)
+                {
+                    ans.insert(0,st.top());
+                }
+                else
+                {
+                    cnt--;
+                }
+                st.pop();
+            }
+        }
+        
+        if(ans[0]!='/')
+        {
+            ans.insert(0,"/");
+        }
+        return ans;
+        
+    }
+        
+};
